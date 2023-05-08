296632b444640e294a88f6d4b0807d3977901f07
Joao-vap <joaovictorpimenta@usp.br>
2023-05-08 12:25:57 -0300

    micro-macro

---
+++ Resumo/code/DysonBridge.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+n_particles = 20
+steps = 1000
+
+def GUE(N, sigma = 1, mu = 0, escale = False):
+    beta = 2
+    A = (np.random.randn(N,N) * sigma + mu)+ 1j*(np.random.randn(N,N) * sigma + mu)
+    A = (A + A.T.conj())/2
+    eig = np.linalg.eigvalsh(A)
+    if escale:
+        return eig/(np.sqrt(N*beta))
+    return eig
+
+eig_GUE = np.zeros(n_particles)
+
+final_time = 1
+dt = final_time/steps
+Memory = np.zeros((steps+1,n_particles))
+
+for step in range(steps+1):
+    partial_time = step * dt
+    sigma = 2*partial_time*(final_time-partial_time)/final_time
+    mu = 0
+    eig_GUE = GUE(n_particles, sigma = sigma, mu = mu, escale = False)
+    eig_order = np.argsort(eig_GUE)
+    Memory[step,:] = eig_GUE[eig_order]
+
+np.savetxt('Memory.txt', Memory)
